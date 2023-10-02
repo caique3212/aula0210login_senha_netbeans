5f34143ac3f24397dec6bf9f6738fb408a0d6a6c
caique coelho <caiqueramos.2240@aluno.saojudas.br>
2023-10-02 20:58:26 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/com/ustj/_usjt_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ustj._usjt_sistema_academico;
+
+/**
+ *
+ * @author 82312240
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
